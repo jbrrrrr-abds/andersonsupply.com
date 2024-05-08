233a53c27c254ed2697cfe51bb1d2fe37827ae28
Justin Burrow <justinburrow@gmail.com>
2024-05-08 01:49:33 -0700

    middleware logging, pt.ii

--- src/app/(client-app)/lib/supabase/middleware.js
+++ src/app/(client-app)/lib/supabase/middleware.js
@@ -57,9 +57,11 @@ export async function updateSession(request) {
     }
   )
   const user = await supabase.auth.getUser()
+  console.log(user);
 
   if (user.data.user) {
     const {data} = await supabase.from('users').select('prismicSlug').eq('email', user.data.user.email);
+    console.log(data[0]);
     if (data[0].prismicSlug) {
       response.headers.set('designArchive', data[0].prismicSlug);
       return response
