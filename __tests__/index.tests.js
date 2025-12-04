3a281346308c9c84b4049efec1aade629a0cb19e
Justin Burrow <justinburrow@gmail.com>
2025-12-04 09:46:13 -0800

    removed unnecesary dummy jest test script

--- __tests__/index.tests.js
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom';
-import { render, screen, waitForElement } from '@testing-library/react';
-import Index from '../src/pages/index.js';
-
-  test('Index should render', () => {
-    render(<Index />)
-  })
