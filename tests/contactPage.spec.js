d384543943294683dae07a4e10696b2ca4002c1e
Justin Burrow <justinburrow@gmail.com>
2025-12-04 17:54:45 -0800

    contact page playwright spec

---
+++ tests/contactPage.spec.js
@@ -0,0 +1,17 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('screenshots', async ({ page}) => {
+  await page.goto('/contact');
+  await expect(page).toHaveScreenshot({maxDiffPixels: 10});
+
+});
+
+test('nav from home to contact', async ({ page }) => {
+  await page.goto('/');
+  await page.getByText('Start Now').click();
+  // Expect that the contact page is navigated to
+  await page.waitForURL('**/contact')
+  await expect(page).toHaveTitle(/Contact/)
+
+});
