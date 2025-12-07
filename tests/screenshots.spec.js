fbea114199c849e8af36383ee62406d661f9bfda
Justin Burrow <justinburrow@gmail.com>
2025-12-06 18:42:33 -0800

    set playwright tests with a comfortable matching threshold expectation for snapshots

--- tests/screenshots.spec.js
+++ tests/screenshots.spec.js
@@ -1,37 +1,88 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('screenshots', async ({page}) => {
-  await page.goto('/');
-  await expect(page).toHaveScreenshot();
+test.use({ viewport: { width: 1400, height: 700 } });
 
-  await page.goto("/services");
-  await expect(page).toHaveScreenshot();
+test.describe("match site page screenshots", () => {
+  test("homepage", async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: false,
+      maxDiffPixelRatio: 0.2,
+    });
+  });
 
-  await page.goto("/process");
-  await expect(page).toHaveScreenshot();
+  test("services-all", async ({ page }) => {
+    await page.goto("/services");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      maxDiffPixelRatio: 0.05,
+    });
+  });
 
-  await page.goto("/work");
-  await expect(page).toHaveScreenshot();
+  test("process", async ({ page }) => {
+    await page.goto("/process");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      maxDiffPixelRatio: 0.05,
+    });
+  });
 
-  await page.goto("/about");
-  await expect(page).toHaveScreenshot();
+  test("work", async ({ page }) => {
+    await page.goto("/work");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      maxDiffPixelRatio: 0.05,
+    });
+  });
 
-  await page.goto("/screen-printing");
-  await expect(page).toHaveScreenshot();
+  test("screen-printing", async ({ page }) => {
+    await page.goto("/screen-printing");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      maxDiffPixelRatio: 0.05,
+    });
+  });
 
-  await page.goto("/headwear");
-  await expect(page).toHaveScreenshot();
+  test("headwear", async ({ page }) => {
+    await page.goto("/headwear");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      maxDiffPixelRatio: 0.05,
+    });
+  });
 
-  await page.goto("/accessories");
-  await expect(page).toHaveScreenshot();
+  test("accessories", async ({ page }) => {
+    await page.goto("/accessories");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      maxDiffPixelRatio: 0.1,
+    });
+  });
 
-  await page.goto("/cut-and-sew");
-  await expect(page).toHaveScreenshot();
+  test("cut-and-sew", async ({ page }) => {
+    await page.goto("/cut-and-sew");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      maxDiffPixelRatio: 0.05,
+    });
+  });
 
-  await page.goto("/screen-printing");
-  await expect(page).toHaveScreenshot();
-
-  await page.goto("/contact");
-  await expect(page).toHaveScreenshot();
+  test("contact", async ({ page }) => {
+    await page.goto("/contact");
+    await expect(page).toHaveScreenshot({
+      animations: "disabled",
+      fullPage: true,
+      maxDiffPixelRatio: 0.05,
+    });
+  });
 });
+
