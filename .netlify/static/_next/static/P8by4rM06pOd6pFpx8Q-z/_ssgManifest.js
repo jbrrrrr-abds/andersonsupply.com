8a351d909eedb7d055c5283ea533e5eb2ed3f9d6
Justin Burrow <justinburrow@gmail.com>
2025-12-18 12:45:42 -0800

    re-adding missing form fields in the dummy Netlify form shell

--- .netlify/static/_next/static/P8by4rM06pOd6pFpx8Q-z/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fabout","\u002Fconfirmed","\u002Fcontact","\u002Fprocess","\u002Fservices","\u002Fservices\u002F[uid]","\u002Fwork"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- src/util/netlifySubmit.js
+++ src/util/netlifySubmit.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 /**
  * send form values to netlify
