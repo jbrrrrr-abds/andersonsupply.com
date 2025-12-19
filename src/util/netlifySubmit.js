386723dbf1c31ce0df97af3e750668a21539a29c
Justin Burrow <justinburrow@gmail.com>
2025-12-18 17:06:52 -0800

    reverting back to last known working version of netlify form submit

--- src/util/netlifySubmit.js
+++ src/util/netlifySubmit.js
@@ -1,10 +1,11 @@
+import axios from "axios";
+
 /**
  * send form values to netlify
  * @param {object} values
  * @param {Array} fileInputNames - names of any inputs that contain files
  * @returns {Promise}
  */
-
 const netlifySubmit = async (values, fileInputNames = []) => {
   const keys = Object.keys(values);
   const formData = new FormData();
@@ -19,19 +20,15 @@ const netlifySubmit = async (values, fileInputNames = []) => {
       });
     } else {
       formData.append(inputName, inputValue);
-      console.log(formData);
     }
   }
 
-  return fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+  return axios({
+    method: "post",
     url: "/__forms/contact.html",
-    body: new URLSearchParams(formData).toString(),
+    headers: { "Content-Type": "multipart/form-data" },
     data: formData,
-  })
-    .then(() => console.log("Form successfully submitted"))
-    .catch((error) => alert(error));
+  });
 };
 
 export default netlifySubmit;
