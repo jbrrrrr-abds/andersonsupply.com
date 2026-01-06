3206dc74dc1b8511a4b7876772b8aefac29da1e7
Justin Burrow <justinburrow@gmail.com>
2026-01-06 04:12:05 -0800

    adding brands we trust page

--- src/pages/trusted-brands/index.js
+++ src/pages/trusted-brands/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import { Helmet } from "react-helmet";
 import { createClient } from "prismicio";
-import Hero from 'components/process/Hero';
-import Modal from 'components/Modal';
-import { useModal } from 'components/Modal/GullsModal';
-import YouTubeModal from 'components/Modal/YouTubeModal';
+import Hero from "components/process/Hero";
+import Modal from "components/Modal";
+import PartnerBrands from "components/PartnerBrands";
+import { useModal } from "components/Modal/GullsModal";
+import YouTubeModal from "components/Modal/YouTubeModal";
 
 const Index = ({ page }) => {
   const { modalOpen } = useModal();
@@ -18,13 +19,19 @@ const Index = ({ page }) => {
       <Helmet
         title={data?.page_title}
         meta={[
-          { name: 'description', content: data?.page_description },
-          { property: 'og:title', content: data?.page_title },
-          { property: 'og:description', content: data?.page_description },
-          { property: 'og:image', content: `${data?.page_social_image?.url}&w=1200&h=630&fit=crop&q=85&f=center` },
-          { name: 'twitter:image', content: `${data?.page_social_image?.url}&w=1200&h=630&fit=crop&q=85&f=center` },
-          { name: 'twitter:title', content: data?.page_title },
-          { name: 'twitter:description', content: data?.page_description },
+          { name: "description", content: data?.page_description },
+          { property: "og:title", content: data?.page_title },
+          { property: "og:description", content: data?.page_description },
+          {
+            property: "og:image",
+            content: `${data?.page_social_image?.url}&w=1200&h=630&fit=crop&q=85&f=center`,
+          },
+          {
+            name: "twitter:image",
+            content: `${data?.page_social_image?.url}&w=1200&h=630&fit=crop&q=85&f=center`,
+          },
+          { name: "twitter:title", content: data?.page_title },
+          { name: "twitter:description", content: data?.page_description },
         ]}
       />
       <main>
@@ -35,10 +42,36 @@ const Index = ({ page }) => {
           image={data?.hero_image}
           videoId={data?.hero_video_id}
         />
+        <PartnerBrands brands={data?.brand_container} />
       </main>
       <Modal>
         <YouTubeModal youTubeID={modalOpen} />
       </Modal>
+      <style jsx>
+        {`
+        .support-grid {
+          display: grid;
+          grid-template-columns: repeat(6, 1fr);
+          padding: var(--spacing);
+          gap: calc(var(--spacing) / 2);
+
+          .card {
+            grid-column: span 2;
+            padding: calc(var(--spacing) / 4);
+
+            @media screen and (width <= 800px) {
+              grid-column: span 6;
+              border-bottom: 1px solid var(--gold);
+              padding-bottom: calc(var(--spacing) / 2);
+              margin-bottom: calc(var(--spacing) / 2);
+
+            ul li a {
+              text-decoration: underline;
+            }
+          }
+        }
+      `}
+      </style>
     </>
   );
 };
